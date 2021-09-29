8897ac5c007db7cc499331666325010f119e75a4
Victor Torres <victor.torrest@ciencias.unam.mx>
2021-09-29 11:41:18 -0500

    Arbol binario con hilos

--- Practica2/sinHilos/babinario.c
+++ Practica2/sinHilos/babinario.c
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <pthread.h>
 #include "tiempo.h"
 
 //Estructura del arbol binario
@@ -48,7 +49,7 @@ int isempty(struct node* root){return root == NULL;}
  * los elementos mas pequeños van en el subarbol izquierdo, y los mas grandes en el derecho.
  *
  * @param un arbol binario y el elemento a insertar
- * @return un arbol binario con el nuevo elemento.
+ * @return un arbol binario con el nuevo elemento insertado.
  */
 struct node* push(int dataToInsert,struct node* root)
 {
@@ -73,11 +74,10 @@ struct node* push(int dataToInsert,struct node* root)
  *
  * va comparando el elemento de la raiz con el numero a buscar y si es mas grande 
  * el nodo entonces recorre a su subarbol izquierdo y sino al derecho. asi hasta encontrar
- * el numero o llegar a un nodo vacio
+ * el numero o llegar a un nodo vacio, al final nos dice si el numero se encontro o no.
  *
- * @param un arbol binario, un arreglo y la variable que
- * nos indica que posicion del arreglo guardaremos.
- * @return un arbol binario con el nuevo elemento
+ * @param un arbol binario y la llave a buscar
+ * @return void
  */
 void search(struct node* root, int key)
 {
@@ -92,14 +92,16 @@ void search(struct node* root, int key)
 
 int main(int argc, char* argv[]){
     double utime0, stime0, wtime0,utime1, stime1, wtime1; //Variables para medición de tiempos
-    int i, element, key;
-    int size = atoi(argv[1]); //Tamaño del arreglo que usaremos
+    int i, element;
+    int size = atoi(argv[1]), key = atoi(argv[2]); //Tamaño del arreglo que usaremos
     struct node* root = NULL; // Inicializar el arbol binario
     //Insertar los n de numeros en el arbol
     for (i = 0; i < size; i++) {
 	scanf("%d", &element);
 	root = push(element, root);
     }
+    printf("\nBusqueda arbol binario (key:%d size:%d).\n\n", key, size);
+
     
     //printf("\nOrdenamiento con arbol binario de busqueda n = %d.\n", size);
     uswtime(&utime0, &stime0, &wtime0); //Iniciamos el conteo
