67c3bdb4708c7b3ef3d6f721fc9594f106bce738
Victor Torres <victor.torrest@ciencias.unam.mx>
2021-10-04 11:20:09 -0500

    Añadiendo graficas de hilos y buscar.c

---
+++ Practica2/sinHilos/buscar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    int size = atoi(argv[1]);
+    int pos = atoi(argv[2]);
+    int i;
+    int* array = malloc(size * sizeof(int));
+    for(i = 0; i < size; i++)
+	scanf("%d", (array+i));
+    printf("%d\n", array[pos-1]);
+    return 0;
+}
