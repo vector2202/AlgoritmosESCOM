529af4e7d3c141798563a19d2c3f009811e0256c
Victor Torres <victor.torrest@ciencias.unam.mx>
2021-12-13 22:12:58 -0600

    Cambios en la animacion y en dp

--- Proyecto/TopDown.c
+++ Proyecto/TopDown.c
@@ -1,33 +1,27 @@
 #include <stdio.h>
-#define NUM_LINE 2
-#define NUM_STATION 4
+#define n 4
 
-// Utility function to find minimum of two numbers
 int min(int a, int b) { return a < b ? a : b; }
 
-int carAssembly(int a[][NUM_STATION], int t[][NUM_STATION], int *e, int *x) {
-  int T1[NUM_STATION], T2[NUM_STATION], i;
+int lineasProduccion(int a[][n], int t[][n], int *e, int *x) {
+  int linea1[n], linea2[n], i;
 
-  T1[0] = e[0] + a[0][0]; // time taken to leave first station in line 1
-  T2[0] = e[1] + a[1][0]; // time taken to leave first station in line 2
+  linea1[0] = e[0] + a[0][0];
+  linea2[0] = e[1] + a[1][0];
 
-  // Fill tables T1[] and T2[] using the above given recursive relations
-  for (i = 1; i < NUM_STATION; ++i) {
-    T1[i] = min(T1[i - 1] + a[0][i], T2[i - 1] + t[1][i] + a[0][i]);
-    T2[i] = min(T2[i - 1] + a[1][i], T1[i - 1] + t[0][i] + a[1][i]);
+  for (i = 1; i < n; ++i) {
+    linea1[i] = min(linea1[i - 1] + a[0][i], linea2[i - 1] + t[1][i] + a[0][i]);
+    linea2[i] = min(linea2[i - 1] + a[1][i], linea1[i - 1] + t[0][i] + a[1][i]);
   }
-
-  // Consider exit times and retutn minimum
-  return min(T1[NUM_STATION - 1] + x[0], T2[NUM_STATION - 1] + x[1]);
+  return min(linea1[n - 1] + x[0], linea2[n - 1] + x[1]);
 }
 
 int main() {
-  int a[][NUM_STATION] = {{4, 5, 3, 2}, {2, 10, 1, 4}};
-  int t[][NUM_STATION] = {{0, 7, 4, 5}, {0, 9, 2, 8}};
+  int a[][n] = {{4, 5, 3, 2}, {2, 10, 1, 4}};
+  int t[][n] = {{0, 7, 4, 5}, {0, 9, 2, 8}};
   int e[] = {10, 12}, x[] = {18, 7};
 
-  printf("%d", carAssembly(a, t, e, x));
+  printf("%d", lineasProduccion(a, t, e, x));
 
   return 0;
 }
-
